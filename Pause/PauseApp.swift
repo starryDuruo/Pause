14ce7ac5cefb0683f34fa6d24fea95902db87a3a
Sige Wang <wangfiz@bc.edu>
2026-04-16 14:02:34 -0400

    Initial Commit

---
+++ Pause/PauseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PauseApp.swift
+//  Pause
+//
+//  Created by Wang Sige on 4/16/26.
+//
+
+import SwiftUI
+
+@main
+struct PauseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
